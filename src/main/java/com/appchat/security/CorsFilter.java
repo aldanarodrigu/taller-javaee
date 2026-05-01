558dd792d8532b77254c42ec6f8446287d3f2c9c
AnaMOrales <ana.morales@estudiantes.utec.edu.uy>
2026-05-01 11:16:49 -0300

    fix cros

--- src/main/java/com/appchat/security/CorsFilter.java
+++ src/main/java/com/appchat/security/CorsFilter.java
@@ -3,19 +3,26 @@ package com.appchat.security;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.container.PreMatching;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
 @Provider
+@PreMatching
 public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", 
+        responseContext.getHeaders().add("Access-Control-Allow-Headers",
             "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", 
+        responseContext.getHeaders().add("Access-Control-Allow-Methods",
             "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            responseContext.setStatus(200);
+        }
     }
 }
