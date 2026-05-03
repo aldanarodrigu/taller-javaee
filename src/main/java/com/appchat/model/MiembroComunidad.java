775889f0f51bf8b2ed22c4abc2117bbd80cd0925
AnaMOrales <ana.morales@estudiantes.utec.edu.uy>
2026-05-02 21:31:53 -0300

    fix

--- src/main/java/com/appchat/model/MiembroComunidad.java
+++ src/main/java/com/appchat/model/MiembroComunidad.java
@@ -16,11 +16,11 @@ public class MiembroComunidad {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "comunidad_id", nullable = false)
     private Comunidad comunidad;
 
